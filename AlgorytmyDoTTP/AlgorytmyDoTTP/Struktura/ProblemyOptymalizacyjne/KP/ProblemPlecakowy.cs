28f34d3c5e47395edf4761b9db7b1c030dc07e60
lqasz <lucas.osuch@wp.pl>
2018-04-05 21:55:27 +0200

    Dodanie Problemu Plecakowego

---
+++ AlgorytmyDoTTP/AlgorytmyDoTTP/Struktura/ProblemyOptymalizacyjne/KP/ProblemPlecakowy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmyDoTTP.Struktura.ProblemyOptymalizacyjne.KP
+{
+    class ProblemPlecakowy
+    {
+        private double wagaPlecaka;
+        private double wartoscPlecaka;
+        private double maxWagaPlecaka;
+
+        public ProblemPlecakowy(Dictionary<int, double> Macierz)
+        {
+            Instancja przedmiot = new Instancja();
+        }
+
+        public void ustawMaxWagePlecaka(double maxWagaPlecaka)
+        {
+            this.maxWagaPlecaka = maxWagaPlecaka;
+        }
+    }
+}
